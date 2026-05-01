3d12168df118efc20dc1fa590c60c55ac43a6640
sksaakeeb <sksakib066@gmail.com>
2026-05-01 22:05:24 +0530

    Update dependencies in backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import path from "path";
+
 import { ENV } from "./lib/env.js";
 
 const app = express();
+
+const __dirname = path.resolve();
+
+// deployment
+if (ENV.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on PORT ${ENV.PORT}`);
